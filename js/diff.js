9bfe4e1a4ce3b0262adf847de3f9d8e1fb315e8d
lanjingling <liuxiao723846@126.com>
2021-07-12 23:05:42 +0800

    增加文本diff可视化功能

---
+++ js/diff.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    $("#sidebyside").click(function(){
+        diffUsingJS(0);
+
+        setLocalStorage('diff.text.input1', $("#baseText").val());
+        setLocalStorage('diff.text.input2', $("#newText").val());
+    });
+
+    $("#inline").click(function(){
+        diffUsingJS(1);
+
+        setLocalStorage('diff.text.input1', $("#baseText").val());
+        setLocalStorage('diff.text.input2', $("#newText").val());
+    });
+
+    //init
+    var val1 = getLocalStorage('diff.text.input1');
+    let val2 = getLocalStorage('diff.text.input2');
+    $("#baseText").val(val1);
+    $("#newText").val(val2);
+});
+
+
+
+function diffUsingJS(viewType) {
+	"use strict";
+	var byId = function (id) { return document.getElementById(id); },
+		base = difflib.stringAsLines(byId("baseText").value),
+		newtxt = difflib.stringAsLines(byId("newText").value),
+		sm = new difflib.SequenceMatcher(base, newtxt),
+		opcodes = sm.get_opcodes(),
+		diffoutputdiv = byId("diffoutput"),
+		contextSize = byId("contextSize").value;
+
+	diffoutputdiv.innerHTML = "";
+	contextSize = contextSize || null;
+
+	diffoutputdiv.appendChild(diffview.buildView({
+		baseTextLines: base,
+		newTextLines: newtxt,
+		opcodes: opcodes,
+		baseTextName: "Base Text",
+		newTextName: "New Text",
+		contextSize: contextSize,
+		viewType: viewType
+	}));
+}
+
+function getLocalStorage(key){
+    var storage=window.localStorage;
+    return storage[key];
+}
+function setLocalStorage(key,val){
+    var storage=window.localStorage;
+    storage[key] = val;
+}
